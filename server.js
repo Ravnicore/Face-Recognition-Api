ad762abf31fabd4306aafeb41fd111093f8c3c09
Ravnicore <kevin.m.kelly@live.com>
2018-10-27 15:30:09 -0400

    changing response

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res)=>{
-	res.send(database.users);
-})
-
+app.get('/', (req, res)=>{ res.send('It is working!') })
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
