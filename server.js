4c687607a4f66d8a4f7671bbb3e5156d9e97a49b
Ravnicore <kevin.m.kelly@live.com>
2018-10-27 16:13:38 -0400

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
   	connection: {
-	    host : '127.0.0.1',
+	    host : 'postgresql-pointy-93577',
 	    user : 'postgres',
 	    password : 'password',
 	    database : 'smart-brain'
