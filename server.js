299a96ef0538291305be840b240c6fcf79bac6bf
Ravnicore <kevin.m.kelly@live.com>
2018-10-27 16:19:11 -0400

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
   	connection: {
-	    host : 'postgresql-pointy-93577',
-	    user : 'postgres',
-	    password : 'password',
-	    database : 'smart-brain'
+	    host : process.env.DATABASE_URL,
+		ssl: true,
 	}
 });
 
